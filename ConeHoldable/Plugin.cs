8d7f9319eb72a92731f54e6ccb9fee8c03833a2b
iiDk <54154105+iiDk-the-actual@users.noreply.github.com>
2025-03-22 23:54:58 -0400

    Add project files.

---
+++ ConeHoldable/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace ConeHoldable
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool done;
+        void Update()
+        {
+            if (done || GorillaTagger.Instance.offlineVRRig == null || GorillaLocomotion.GTPlayer.Instance == null)
+                return;
+
+            OnGameInitialized();
+            done = true;
+        }
+
+        void OnGameInitialized()
+        {
+            GameObject cone = LoadAsset("ConeHold");
+            cone.transform.SetParent(GorillaTagger.Instance.offlineVRRig.transform.Find("RigAnchor/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R"), false);
+        }
+
+        static AssetBundle assetBundle = null;
+        public static GameObject LoadAsset(String assetName)
+        {
+            GameObject gameObject = null;
+
+            if (assetBundle == null)
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ConeHoldable.Resources.conehold");
+                assetBundle = AssetBundle.LoadFromStream(stream);
+                stream.Close();
+            }
+            gameObject = Instantiate<GameObject>(assetBundle.LoadAsset<GameObject>(assetName));
+
+            return gameObject;
+        }
+    }
+}
---
+++ ConeHoldable/StartPatch.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace ConeHoldable
+{
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Awake", MethodType.Normal)]
+    internal class ExamplePatch
+    {
+        private static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            OnGameInitialized();
+        }
+
+        static void OnGameInitialized()
+        {
+            GameObject cone = LoadAsset("ConeHold");
+            cone.transform.SetParent(GorillaTagger.Instance.offlineVRRig.transform.Find("RigAnchor/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R"), false);
+        }
+
+        static AssetBundle assetBundle = null;
+        public static GameObject LoadAsset(String assetName)
+        {
+            GameObject gameObject = null;
+
+            if (assetBundle == null)
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ConeHoldable.Resources.conehold");
+                assetBundle = AssetBundle.LoadFromStream(stream);
+                stream.Close();
+            }
+            gameObject = UnityEngine.Object.Instantiate<GameObject>(assetBundle.LoadAsset<GameObject>(assetName));
+
+            return gameObject;
+        }
+    }
+}
