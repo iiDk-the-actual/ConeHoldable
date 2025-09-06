6f0aff73b0413650168fd055225253a0c0ed1fe1
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-05 21:57:02 -0400

    Update assetbundle

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.gorillatag.coneholdable";
         public const string Name = "ConeHoldable";
-        public const string Version = "1.0.2";
+        public const string Version = "1.0.3";
     }
 }
