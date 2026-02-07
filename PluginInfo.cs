21029ada2c1b31684db4a7c8d585f4455bb1f429
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-02-06 20:30:03 -0500

    1.0.4

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.gorillatag.coneholdable";
         public const string Name = "ConeHoldable";
-        public const string Version = "1.0.3";
+        public const string Version = "1.0.4";
     }
 }
